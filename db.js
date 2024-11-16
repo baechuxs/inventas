610ee2d860197dc913f7c43b37f5dc6cb02d30d5
Panji Anugrah <106466226+baechuxs@users.noreply.github.com>
2024-11-16 18:34:22 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: 'mysql.railway.internal',
   user: 'root',
-  password: '',
-  database: 'taspen',
+  password: 'ztXDFhClrTWixpHDOGcUaFXQyXXEwRGX',
+  database: 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
